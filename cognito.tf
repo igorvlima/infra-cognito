974f2de5e21a2b3ca0776529ad93c74d3c82736f
igorvlima <igorveraslima3@gmail.com>
2025-05-24 19:03:46 -0300

    change cognito config

--- cognito.tf
+++ cognito.tf
@@ -1,33 +1,31 @@
-resource "aws_cognito_user_pool" "user_pool" {
-  name = "cpf-auth-user-pool"
+resource "aws_cognito_user_pool" "lanchonete_user_pool" {
+  name = "lanchonete-user-pool"
+
+  username_attributes = ["email"]
+  alias_attributes    = ["preferred_username"]
+
+  schema {
+    name     = "preferred_username"
+    required = true
+    mutable  = true
+    attribute_data_type = "String"
+  }
 
   auto_verified_attributes = []
-
-  password_policy {
-    minimum_length    = 8
-    require_lowercase = true
-    require_uppercase = true
-    require_numbers   = true
-    require_symbols   = false
-  }
-
-  admin_create_user_config {
-    allow_admin_create_user_only = false
-  }
 }
 
-resource "aws_cognito_user_pool_client" "user_pool_client" {
-  name         = "cpf-auth-client"
-  user_pool_id = aws_cognito_user_pool.user_pool.id
+resource "aws_cognito_user_pool_client" "lanchonete_user_pool_client" {
+  name         = "lanchonete-user-pool-client"
+  user_pool_id = aws_cognito_user_pool.lanchonete_user_pool.id
 
   generate_secret = false
 
   explicit_auth_flows = [
     "ALLOW_USER_PASSWORD_AUTH",
-    "ALLOW_REFRESH_TOKEN_AUTH"
+    "ALLOW_REFRESH_TOKEN_AUTH",
+    "ALLOW_ADMIN_USER_PASSWORD_AUTH"
   ]
 
-  supported_identity_providers = ["COGNITO"]
-
-  allowed_oauth_flows_user_pool_client = false
+  prevent_user_existence_errors = "ENABLED"
+  supported_identity_providers  = ["COGNITO"]
 }
